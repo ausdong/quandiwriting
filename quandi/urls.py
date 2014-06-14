940fbfaa801f2a37f958fe936a9834f4da5c90af
ausdong <aus.dong@gmail.com>
2014-06-14 19:54:32 -0400

    password change tweaks, profile link in header

--- quandi/urls.py
+++ quandi/urls.py
@@ -10,13 +10,14 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
    url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'testsite.views.index', name='home1'),
 	url(r'^About/', 'testsite.views.about', name='about'),
+	url(r'^services/', 'testsite.views.services', name='serv'),
 	url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'testsite/login.html'}),
 	url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 	url(r'^register/', 'testsite.views.register', name='register'),
 	url(r'^profile/', 'testsite.views.profile', name='profile'),
-	url(r'^password_change_done', 'testsite.views.pwchangedone', name='password_change_done'),
-	url(r'^password_change_form/', 'django.contrib.auth.views.password_change', {'post_change_redirect': '/password_change_done'}),
-	url(r'^services/', 'testsite.views.services', name='serv'),
-	url(r'^$', 'testsite.views.index', name='home1'),
+	url(r'^password_change_done/', 'testsite.views.pwchangedone', name='password_change_done'),
+	url(r'^password_change_form/', 'django.contrib.auth.views.password_change', {'template_name': 'testsite/password_change_form.html', 'post_change_redirect': '/password_change_done'}),
+	
 )
